83180eab72c998fc5bf06ad0844f923fa44e60ff
kamalabot <kamaljp@gmail.com>
2023-12-11 17:26:15 +0530

    message collector updated

---
+++ update_message_collector.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+# matrix_synapse_automator_scripts/updated_message_collector.py
+
+"""Doesn't Work. Collects and writes the message from the single room to a file."""
+import asyncio
+from nio import AsyncClient, InviteMemberEvent, RoomMessageText
+from automation_helper import message_callback
+from time import sleep
+
+# Script completes following tasks: 
+# accept the invites automatically
+# write the room_ids and their displayNames 
+
+from dotenv import load_dotenv
+import os
+
+import logging
+
+# preparing the logging config
+logging.basicConfig(format='%(asctime)s | %(levelname)s | %(message)s',
+                    datefmt='%d-%b',
+                    level=logging.INFO)
+
+async def main() -> None:
+    load_dotenv()
+    user = input("Provide the server user id: [@user:dom.sample.in] ")
+    password = input("Provide the password: ") 
+    if user != "":
+        client = AsyncClient(os.environ.get("SERVER"),user)
+        logging.info(await client.login(password))
+    else: 
+        client = AsyncClient(os.environ.get("SERVER"),os.environ.get("MATUSER"))
+        logging.info(await client.login(os.environ.get("PASS")))
+
+        client.add_event_callback(message_callback, RoomMessageText)
+
+        since_token = input("Provide a next_batch token if you have: ")
+
+        if since_token:
+            sync_data = await client.sync(full_state=True, since=since_token)
+        else:
+            sync_data = await client.sync(full_state=True)
+
+        logging.info(f"next_batch_token: {sync_data.next_batch}")
+        await client.close()
+
+asyncio.run(main())
